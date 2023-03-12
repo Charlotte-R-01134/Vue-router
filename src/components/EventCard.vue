8e298bd9c4bb8d4960d0f3b21347c668cad51ee2
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2023-03-12 11:50:03 +0000

    end of l4

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,24 +1,25 @@
-<script setup>
-defineProps({
-  event: {
-    type: Object,
-    required: true,
-  },
-});
-</script>
-
 <template>
-  <RouterLink
+  <router-link
     class="event-link"
     :to="{ name: 'EventDetails', params: { id: event.id } }"
   >
     <div class="event-card">
-      <!-- Display event data -->
-      <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
     </div>
-  </RouterLink>
+  </router-link>
 </template>
+
+<script>
+export default {
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
 
 <style scoped>
 .event-card {
@@ -28,12 +29,14 @@ defineProps({
   border: 1px solid #39495c;
   margin-bottom: 18px;
 }
+
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
+
 .event-link {
-  text-decoration: none;
   color: #2c3e50;
+  text-decoration: none;
 }
 </style>
