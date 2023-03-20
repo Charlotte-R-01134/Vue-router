c785bae2e127fc0810e5c39a654e832c6d41edfd
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2023-03-20 11:21:32 +0000

    finished vue router course

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import GStore from './store'
+import 'nprogress/nprogress.css'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.provide('GStore', GStore)
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ import EventDetails from '../views/event/Details.vue'
 import EventRegister from '../views/event/Register.vue'
 import EventEdit from '../views/event/Edit.vue'
 import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
+import NetworkError from '../views/NetworkError.vue'
+import NProgress from 'nprogress'
+import EventService from '@/services/EventService.js'
+import GStore from '@/store'
 
 const routes = [
   {
@@ -14,10 +19,30 @@ const routes = [
     props: (route) => ({ page: parseInt(route.query.page) || 1 }),
   },
   {
-    path: '/event/:id',
+    path: '/events',
+    redirect: { name: 'EventList' },
+  },
+  {
+    path: '/events/:id',
     name: 'EventLayout',
     props: true,
     component: EventLayout,
+    beforeEnter: (to) => {
+      return EventService.getEvent(to.params.id)
+        .then((response) => {
+          GStore.event = response.data
+        })
+        .catch((error) => {
+          if (error && error.response && error.response.status == 404) {
+            return {
+              name: '404Resource',
+              params: { resource: 'event' },
+            }
+          } else {
+            return { name: 'NetworkError' }
+          }
+        })
+    },
     children: [
       {
         path: '',
@@ -33,19 +58,55 @@ const routes = [
         path: 'edit',
         name: 'EventEdit',
         component: EventEdit,
+        meta: { requiresAuth: true}
       },
     ],
   },
   {
-    path: '/about',
+    path: '/event/:afterEvent(.*)',
+    redirect: (to) => {
+      return { path: '/events/' + to.params.afterEvent }
+    },
+  },
+  {
+    path: '/about-us',
     name: 'About',
     component: About,
+    // alias: '/about',
+  },
+  {
+    path: '/about',
+    redirect: { name: 'About' },
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true,
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError,
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+})
+
+router.beforeEach(() => {
+  NProgress.start()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
